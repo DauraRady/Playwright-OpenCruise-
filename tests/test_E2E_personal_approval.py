630c39f9975368bc97b224e8033925e3e23872de
DR <dorra.rady@gmail.com>
2025-05-15 02:08:19 +0200

    Update test_E2E_personal_approval.py

--- tests/test_E2E_personal_approval.py
+++ tests/test_E2E_personal_approval.py
@@ -19,24 +19,24 @@ def test_e2e_personal_approval(page, base_url,credentials_par, admin_credentials
     generated_email = fake.email()
     personal_data["email"] = generated_email
 
-    # Création du compte particulier
+    
     page.goto(f"{base_url}/login?returnUrl=%2F")
     personal_page = PersonalRegistrationPage(page)
     personal_page.navigate_to_registration()
     personal_page.fill_form(personal_data)
     personal_page.submit_form()
-    # Stockage des données générées pour les réutiliser plus tard dans les autres tests
+  
     credentials_par["email"] = generated_email
     credentials_par["password"] = personal_data["password"]  # mot de passe du JSON
 
     page.screenshot(path="screenshot_after_par_account_fill.png")
 
-     # 2. Vérifier que le site redirige vers la page de login après la création du compte
+  
     page.wait_for_url(re.compile(".*login.*"), timeout=5000)
     assert "login" in page.url, "La redirection vers la page de login n'a pas eu lieu après la création du compte professionnel."
     page.screenshot(path="screenshot_after_par_account_creation.png")
 
-    # Connexion en tant qu'admin et approbation du compte créé
+
     admin_page = AdminManagementPage(page)
     admin_page.goto(base_url)
     admin_page.login(admin_credentials["username"], admin_credentials["password"])
@@ -45,12 +45,12 @@ def test_e2e_personal_approval(page, base_url,credentials_par, admin_credentials
     admin_page.logout()
 
     page.screenshot(path="screenshot_e2e_personal_approval.png")
- # 5) Connexion avec le compte pra créé
+ 
     page.get_by_role("textbox", name="Nom de l'utilisateur").fill(personal_data["email"])
     page.get_by_role("textbox", name="Mot de passe").fill(personal_data["password"])
     page.get_by_role("button", name="Connexion").click()
 
-    # Vérifier la connexion
+
     page.wait_for_selector("text=Bienvenue", timeout=5000)
     assert "Bienvenue" in page.content(), "La connexion avec le compte particulier créé a échoué."
 
