4b17929bb7cd43a23af3cebac5da6fcf26dcfe36
DR <dorra.rady@gmail.com>
2026-03-24 01:59:38 +0100

    Refactor login attempt test for clarity
    Removed unnecessary comment and adjusted code formatting.

--- tests/test_E2E_personal_login_attempts.py
+++ tests/test_E2E_personal_login_attempts.py
@@ -6,7 +6,7 @@ def test_blocage_apres_5_tentatives(page, credentials_par,
         page.get_by_role("textbox", name="Nom de l'utilisateur").fill(credentials_par["email"])
         page.get_by_role("textbox", name="Mot de passe").fill("motdepassefaux")
         page.get_by_role("button", name="Connexion").click()
-        # Vérifier clairement que l'alerte est bien visible
+        
         assert page.get_by_role("alertdialog", name="mot de passe ou identifiant").is_visible(), \
         "L'alerte 'mot de passe ou identifiant' n'est pas affichée."
 
@@ -16,7 +16,7 @@ def test_blocage_apres_5_tentatives(page, credentials_par,
     page.get_by_role("textbox", name="Nom de l'utilisateur").fill(credentials_par["email"])
     page.get_by_role("textbox", name="Mot de passe").fill(credentials_par["password"])
     page.get_by_role("button", name="Connexion").click()
-    page.wait_for_load_state("networkidle")  # Attend que toutes les requêtes soient terminées
+    page.wait_for_load_state("networkidle")  
     page.screenshot(path="screenshot_attemps_pro_ko_approval.png")
     assert page.get_by_role("alertdialog", name="votre compte est bloqué").is_visible(), "Le compte n'est pas bloqué après 5 tentatives."
     
